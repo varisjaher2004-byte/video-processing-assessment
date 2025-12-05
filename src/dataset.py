e2701f51ba84c2f538600747d15b1e2ad5218640
varisjaher2004-byte <varisjaher2004@gmail.com>
2025-12-05 18:42:20 +0000

    Enhance VideoDataset with frame loading and transformations
    Refactor VideoDataset class to inherit from Dataset and add frame loading functionality.

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,44 @@
-# Dataset loader for video frames, captions and tags
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
 
-class VideoDataset:
-    def __init__(self):
-        pass
+class VideoDataset(Dataset):
+    def __init__(self, frame_dirs, captions, tags, transform=None):
+        self.frame_dirs = frame_dirs          # list of folders
+        self.captions = captions              # list of captions
+        self.tags = tags                      # list of semantic tags
+        self.transform = transform or transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
 
+    def load_frames(self, folder_path):
+        frames = []
+        files = sorted(os.listdir(folder_path))  # sorted list of frame names
+
+        for f in files:
+            if f.endswith(".jpg") or f.endswith(".png"):
+                img_path = os.path.join(folder_path, f)
+                img = Image.open(img_path).convert("RGB")
+                img = self.transform(img)
+                frames.append(img)
+
+        return frames
+
+    def __getitem__(self, idx):
+        folder = self.frame_dirs[idx]
+        caption = self.captions[idx]
+        tag = self.tags[idx]
+
+        frames = self.load_frames(folder)
+
+        return {
+            "frames": frames,
+            "caption": caption,
+            "tags": tag,
+        }
+
+    def __len__(self):
+        return len(self.frame_dirs)
